8b4ea352815015197ae7afa0146397100587c477
AriesRedWar <philipjbowman5@gmail.com>
2022-09-20 18:50:25 -0400

    added DataContext settings to the App.js and Gallery.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
-import { useEffect, useState } from 'react'
-import Gallery from './components/Gallery'
-import SearchBar from './components/SearchBar'
-import './App.css'
+import { useEffect, useState } from "react";
+import Gallery from "./components/Gallery";
+import SearchBar from "./components/SearchBar";
+import { DataContext } from "./context/DataContext";
+
+import "./App.css";
 
 // function App (){}
 const App = () => {
-  let [search, setSearch] = useState('')
-  let [message, setMessage] = useState('Search for Music!')
-  let [data, setData] = useState([])
+  let [search, setSearch] = useState("");
+  let [message, setMessage] = useState("Search for Music!");
+  let [data, setData] = useState([]);
 
-  const API_URL = 'https://itunes.apple.com/search?term='
+  const API_URL = "https://itunes.apple.com/search?term=";
 
   useEffect(() => {
-      if(search) {
-          const fetchData = async () => {
-              document.title = `Search results for ${search} music`
-              setMessage(`Search results for: ${search}`)
-              const response = await fetch(API_URL + search)
-              const resData = await response.json()
-              if (resData.results.length > 0) {
-                  return setData(resData.results)
-              } else {
-                  return setMessage('Not Music found for the search term.')
-              }
-          }
-          fetchData()
-      }
-  }, [search])
+    if (search) {
+      const fetchData = async () => {
+        document.title = `Search results for ${search} music`;
+        setMessage(`Search results for: ${search}`);
+        const response = await fetch(API_URL + search);
+        const resData = await response.json();
+        if (resData.results.length > 0) {
+          return setData(resData.results);
+        } else {
+          return setMessage("Not Music found for the search term.");
+        }
+      };
+      fetchData();
+    }
+  }, [search]);
 
   const handleSearch = (e, term) => {
-      e.preventDefault()
-      setSearch(term)
-  }
+    e.preventDefault();
+    setSearch(term);
+  };
 
   return (
-      <div>
-          <h1 className='App'>{message}</h1>
-          <SearchBar handleSearch={handleSearch} />
-          <Gallery data={data} />
-      </div>
-  )
-}
+    <div>
+      <h1 className="App">{message}</h1>
+      <SearchBar handleSearch={handleSearch} />
+      <DataContext.Provider value={data}>
+        <Gallery />
+      </DataContext.Provider>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,18 +1,15 @@
-import GalleryItem from './GalleryItem'
+import { useContext } from "react";
+import { DataContext } from "../context/DataContext";
+import GalleryItem from "./GalleryItem";
 
-function Gallery(props){
+const Gallery = () => {
+  const data = useContext(DataContext);
 
-    const display = props.data.map((item, index) => {
-        return (
-            <GalleryItem item={item} key={index} />
-        )
-    })
+  const display = data.map((item, index) => {
+    return <GalleryItem key={index} item={item} />;
+  });
 
-    return (
-        <div>
-            {display}
-        </div>
-    )
-}
+  return <div>{display}</div>;
+};
 
-export default Gallery
+export default Gallery;
